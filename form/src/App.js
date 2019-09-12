2c2263729e8e23d150fd77225d5bdd5bfc7a7c9f
Dil Sombayeva <dilyasom@gmail.com>
2019-09-12 10:28:12 -0700

    basic form

--- form/src/App.js
+++ form/src/App.js
@@ -3,13 +3,23 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <form action="">
-          <input />
-      </form>
-    </div>
-  );
+ return (
+  <div className="App">
+   <form action="">
+    <label>
+     Username
+     <input />
+    </label>
+    <input type="button" value="button" />
+    <input type="text" placeholder="Enter your username" />
+    <input type="date" />
+    <input type="file" />
+    <input type="password" />
+    <input type="radio" value="D" />
+    <input type="checkbox" />
+   </form>
+  </div>
+ );
 }
 
 export default App;
